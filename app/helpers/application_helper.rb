05b58cd68aa720a8d6dab28ae0552382cbb427af
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-14 18:18:50 +0000

    automatically backed up by learn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  module ClassMethods
+    def highest_ratio_res_to_listings
+      self.has_listings.max_by {|location| location.reservations.count / location.listings.count}
+    end
+
+    def most.res
+      self.has_listings.max_by {|location| location.reservations.count}
+    end
+
+    def has_listings
+      self.all.select {|location| location.listings.any?}
+    end
+  end
 end
